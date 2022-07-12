a7efb80cbdd1c92230ba97b295e25f0bf095deb6
Miljan Vukićević <vukicevicmiljan92@gmail.com>
2022-07-12 18:34:17 +0200

    Initial commit

---
+++ StarterProject/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  StarterProject
+//
+//  Created by Miljan Vukicevic on 12.7.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
